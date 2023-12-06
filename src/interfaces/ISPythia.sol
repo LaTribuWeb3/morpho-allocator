d1a9f2e5e8a9adb32e3f43f7b9abb72a0ad1610a
Alfu <Alfulinku@gmail.com>
2023-12-06 16:03:39 +0100

    wip

--- src/interfaces/ISPythia.sol
+++ src/interfaces/ISPythia.sol
@@ -1,21 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
 
-interface ISPythia {
-    struct RiskData {
-        address collateralAsset;
-        address debtAsset;
-        uint256 liquidity;
-        uint256 volatility;
-        uint256 lastUpdate;
-        uint256 chainId;
-    }
+struct RiskData {
+    address collateralAsset;
+    address debtAsset;
+    uint256 liquidity;
+    uint256 volatility;
+    uint256 lastUpdate;
+    uint256 chainId;
+}
 
-    struct Signature {
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-    }
+struct Signature {
+    uint8 v;
+    bytes32 r;
+    bytes32 s;
+}
+
+interface ISPythia {
 
     function getSigner(RiskData memory data, uint8 v, bytes32 r, bytes32 s) external returns(address);
 }
